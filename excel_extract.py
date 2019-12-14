dbeb121e4ba21332dbe3e9d92a69e937982823ad
klisly <wizardholy@163.com>
2019-12-15 00:54:52 +0800

    init learn data

--- excel_extract.py
+++ excel_extract.py
@@ -4,6 +4,7 @@
 # @Author  : guifeng(guifeng.mo@eyespage.cn)
 # @File    : excel.py
 import pandas as pd
+from sklearn.metrics import *
 
 datas = pd.read_excel('/Users/wizardholy/Documents/baice/up/用户画像 规划 (1).xlsx')
 print(datas)
@@ -11,3 +12,15 @@ print(datas)
 import numpy as np
 
 from scipy import spatial
+
+import numpy as np
+
+
+def mse(true, pred):
+    return np.sum((true - pred) ** 2)
+
+
+def mas(true, pred):
+    return np.sum(np.abs(true - pred))
+
+from sklearn.metrics import classification_report
